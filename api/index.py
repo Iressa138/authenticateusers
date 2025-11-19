94e57b171071ddd1fe6ca9784fb0e8f09f783669
Iressa138 <Deadoppzz856@gmail.com>
2025-11-19 14:09:06 -0500

    Remove docstring from give_access endpoint
    Removed docstring from give_access function.

--- api/index.py
+++ api/index.py
@@ -10,11 +10,6 @@ ALLOWED_USERS = [
 
 @app.route("/api/giveaccess", methods=["GET"])
 def give_access():
-    """
-    Returns a JSON object containing the list of allowed users.
-    Format matches Unity's BackendList class:
-    { "users": ["user_1", "user_2", ...] }
-    """
     return jsonify({"users": ALLOWED_USERS})
 
 if __name__ == "__main__":
