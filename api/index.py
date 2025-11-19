6d2fa200dcd964ee2daab65a74f8d19128de60e3
Iressa138 <Deadoppzz856@gmail.com>
2025-11-19 14:05:55 -0500

    Create /api/giveaccess endpoint for user access
    Added a new API endpoint to return allowed users.

--- api/index.py
+++ api/index.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+ALLOWED_USERS = [
+    "6337049A5066F60A",
+    "DBD8B6CBA6FB98D8",
+    "user_3"
+]
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/api/giveaccess", methods=["GET"])
+def give_access():
+    """
+    Returns a JSON object containing the list of allowed users.
+    Format matches Unity's BackendList class:
+    { "users": ["user_1", "user_2", ...] }
+    """
+    return jsonify({"users": ALLOWED_USERS})
+
+if __name__ == "__main__":
+    app.run(debug=True)
